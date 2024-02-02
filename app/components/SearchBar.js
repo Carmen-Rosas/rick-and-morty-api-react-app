4284da752dc60e58ac3d7263bce6f94ffa1815e7
Carmen-Rosas <carmen.rosas.cancio.suarez@gmail.com>
2024-02-02 14:54:05 +0100

    search buttons

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,9 +1,15 @@
 
 export function SearchBar() {
     return (
-        <form className="searchBar" onsubmit="" role="search">
-            <input type="search" placeholder="Search..." autofocus="autofocus" />
-            <button className="searchButton" type="submit">Go</button>  
-        </form>
+        <div className="search">
+            <form className="searchBar" onsubmit="" role="search">
+                <input type="search" placeholder="Search..." autofocus="autofocus" />
+                <button className="searchButton" type="submit">Go</button>  
+            </form>
+            <button className="optionsButton">Character</button>
+            <button className="optionsButton">Location</button>
+            <button className="optionsButton">Episode</button>
+        </div>
+        
     )
 }

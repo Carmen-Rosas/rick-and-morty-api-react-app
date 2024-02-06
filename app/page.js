27289fbf074ae371b73bd8ba43754bbd1de549b5
Carmen-Rosas <113897566+Carmen-Rosas@users.noreply.github.com>
2024-02-06 14:59:10 +0100

    next button

--- app/page.js
+++ app/page.js
@@ -2,14 +2,19 @@
 import "./page.css";
 import "./components/components.css";
 import {useFetch} from "./useFetch";
+import {useState, useEffect} from "react";
 import {CharacterCards} from "./components/Cards";
 import {Header} from "./components/Header";
 import {SearchBar} from "./components/SearchBar";
 import {Footer} from "./components/Footer";
 
 export default function Home() {
-  const characters = useFetch("https://rickandmortyapi.com/api/character");
-  const locations = useFetch("https://rickandmortyapi.com/api/location");
+  const [currentPage, setCurrentPage] = useState(1);
+  const characters = useFetch(currentPage);
+
+  function handleClick() {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <>
@@ -24,6 +29,9 @@ export default function Home() {
             <CharacterCards character={character}/>
           </div>
         ))}
+    </div>
+    <div>
+      <button onClick={handleClick}>Next</button>
     </div>
     <Footer/>
     </>
--- app/useFetch.js
+++ app/useFetch.js
@@ -1,13 +1,13 @@
 import {useState, useEffect} from "react";
 
-export function useFetch(url) {
+export function useFetch(currentPage) {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
             .then((response) => response.json())
             .then((data) => setData(data));
-    }, []);
+    }, [currentPage]);
 
     return data;
 }

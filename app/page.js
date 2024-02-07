fd24fd61cdd1b0174139c537baf33d5ddef56788
Carmen-Rosas <113897566+Carmen-Rosas@users.noreply.github.com>
2024-02-07 11:42:23 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,39 +1,46 @@
 "use client";
 import "./page.css";
 import "./components/components.css";
-import {useFetch} from "./useFetch";
-import {useState, useEffect} from "react";
-import {CharacterCards} from "./components/Cards";
-import {Header} from "./components/Header";
-import {SearchBar} from "./components/SearchBar";
-import {Footer} from "./components/Footer";
+import { useFetch } from "./useFetch";
+import { useState, useEffect } from "react";
+import { CharacterCards } from "./components/Cards";
+import { Header } from "./components/Header";
+import { SearchBar } from "./components/SearchBar";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const characters = useFetch(currentPage);
+  
+  function handleClickPrev() {
+    if (characters.info.prev != null)
+      setCurrentPage(currentPage - 1);
+  };
 
-  function handleClick() {
-    setCurrentPage(currentPage + 1);
+  function handleClickNext() {
+    if (characters.info.next != null)
+      setCurrentPage(currentPage + 1);
   };
 
   return (
     <>
-    <Header title="Rick and Morty"/>
-    <div className="line"></div>
-    <div className="space">
-      <SearchBar/>
-    </div>
-    <div className="homeCard">
+      <Header title="Rick and Morty" />
+      <div className="line"></div>
+      <div className="space">
+        <SearchBar />
+      </div>
+      <div className="homeCard">
         {characters?.results.map((character) => (
           <div key={character.id}>
-            <CharacterCards character={character}/>
+            <CharacterCards character={character} />
           </div>
         ))}
-    </div>
-    <div>
-      <button onClick={handleClick}>Next</button>
-    </div>
-    <Footer/>
+      </div>
+      <div>
+        <button onClick={handleClickPrev}>Previous</button>
+        <button onClick={handleClickNext}>Next</button>
+      </div>
+      <Footer />
     </>
   );
 }
